211c399e8920c86eec931f0e01326d52a620ca81
nicolechuque <universidadnicole27@gmail.com>
2026-06-15 20:26:00 -0500

    add images to sidebar and main area

--- app.py
+++ app.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
+
+st.image("Python_logo_and_wordmark.svg.png")
+st.sidebar.image("logo-secundario-dmc-institute-01.png")
