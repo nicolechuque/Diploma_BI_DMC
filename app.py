9d93c1c080ce1e2b63ce46d7199fafd500d2cc20
nicolechuque <universidadnicole27@gmail.com>
2026-06-15 20:28:05 -0500

    add images to sidebar and main area

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ st.title("Proyecto Final Diploma BI")
 
 st.sidebar.title("Parámetros")
 
-st.image("Python_logo_and_wordmark.svg.png")
-st.sidebar.image("logo-secundario-dmc-institute-01.png")
+st.image("Python_logo_and_wordmark.svg.png",width = 50 )
+st.sidebar.image("logo-secundario-dmc-institute-01.png",width = 50 )
