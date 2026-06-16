e0ad1e206a9e3d21178be0eb849df98b3ff72f22
nicolechuque <universidadnicole27@gmail.com>
2026-06-15 20:32:50 -0500

    add name

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.sidebar.title("Parámetros")
 
 st.image("Python_logo_and_wordmark.svg.png",width = 500)
 st.sidebar.image("logo-secundario-dmc-institute-01.png",width = 300)
+
+st.write("Elaborado por: Nicole Chuque")
