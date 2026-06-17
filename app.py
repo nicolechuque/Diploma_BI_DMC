12b85da050abab9923d89078c16842618bbd3ea1
nicolechuque <universidadnicole27@gmail.com>
2026-06-17 14:37:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,17 @@ st.sidebar.image("logo-secundario-dmc-institute-01.png",width = 300)
 st.write("Elaborado por: Nicole Chuque")
 
 archivo = st.file_uploader("Cargue el archivo excel o csv")
+
+if archivo is not None :
+  
+  if archivo.name.endswith(".csv")
+      data = pd. read_csv(archivo)
+      st.write(data)
+  elif archivo.name.endswith(".xlsx")
+     data = pd. read_csv(archivo)
+      st.write(data)
+  else:
+    st.write("Formato no válido")
+    
+else :
+  st.write("Por favor cargue su archivo")
